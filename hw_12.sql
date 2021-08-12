7c821ada816b6891d29732b6ecd7d30a56d3a18a
Vlad Google <vladittrello@gmail.com>
2021-08-12 11:33:26 +0300

    HW_12 v.2.0

--- hw_12.sql
+++ hw_12.sql
@@ -19,12 +19,44 @@ CREATE TABLE if not exists house_builder_relation(
     UNIQUE (id_house, id_builder)
     );
 
+CREATE OR REPLACE FUNCTION set_salary() RETURNS TRIGGER AS $$
+BEGIN
+
+if new.position like 'Build%' then
+new.salary:= 15000;
+return new;
+
+elseif new.position like 'Crane%' then
+new.salary:= 30000;
+return new;
+
+
+elseif new.position like 'Task%' then
+new.salary:= 60000;
+return new;
+
+else new.salary:= 0;
+return new;
+end if;
+
+end;
+$$ LANGUAGE plpgsql;
+
+drop trigger if exists salary on builder;
+
+create trigger salary
+    before insert or update on builder
+                         for each row
+                         execute procedure set_salary();
+
+
+
 
 
 
 insert into project(status) values('On build'),('Finished'), ('Prepare for building');
 insert into house(floors, id_obj) values('50', 1),('170', 1), ('68', 2),('500', 3),('120', 3);
-insert into builder(position, salary) values('Builder №1', 10000),('Builder №2', 15000), ('Builder №3', 20000),('Taskmaster №1', 60000),('Taskmaster №2', 100000);
+insert into builder(position) values('Builder №1'),('Builder №2'), ('Crane operator'),('Taskmaster №1'),('Taskmaster №2');
 insert into clients(age) values (55), (23), (44),(35),(34),(29), (60),(79),(86),(88);
 insert into flat(rooms,price, id_client, id_house) values(3, 50000, 1, 1),(2, 30000, 1, 1), (1, 15000, 10, 2),(3, 150000, 10, 3),(2, 564748, 10, 4), (3, 58589, 5, 4), (2, 85854, 5, 4);
 
@@ -37,5 +69,4 @@ insert into house_builder_relation values(1,1),(1,2),(1,3),(1,4),(2,2),(3,5),(5,
 SELECT house.floors, house.id_obj as project_number, builder.position, builder.salary
 FROM house JOIN house_builder_relation ON house.id_house = house_builder_relation.id_house
            JOIN builder ON house_builder_relation.id_builder = builder.id_builder
-where builder.salary > 10000;
-
+where builder.salary > 15000;
